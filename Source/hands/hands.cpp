350f51c4af15d3ae829425ee7d24d87d55b9f841
YuanWeiChen16 <wficta137@gmail.com>
2023-07-18 21:22:30 +0800

    init

---
+++ Source/hands/hands.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "hands.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, hands, "hands" );
---
+++ Source/hands/handsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "handsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANDS_API AhandsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
